6823a058a97ddd5c1a67530752ce4ed8ef5432b4
shake <hiroki.yamako@ezweb.ne.jp>
2020-12-17 22:07:50 +0900

    add new file

---
+++ output_storn.py
@@ -0,0 +1,18 @@
+import put_storn
+
+puts_storn = put_storn.PutsStorn()
+
+def outputs_storn(board):
+    for y in range(10):
+        for x in range(10):
+            if board[y][x] == 1:
+                print("○ ", end = '')
+                #print('{:^3}'.format(puts_storn.board[y][x]), end = '')
+            elif board[y][x] == -1:
+                print("● ", end = '')
+            elif board[y][x] == 0:
+                print("□ ", end = '')
+            else:
+                print(board[y][x], end = '')
+                print(' ', end = '')
+        print()
