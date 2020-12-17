63556b3df403434eb156cbc36f7e2053a8a63343
shake <hiroki.yamako@ezweb.ne.jp>
2020-12-17 23:10:30 +0900

    本来置けない場所における現象を修正

--- put_storn.py
+++ put_storn.py
@@ -24,6 +24,7 @@ class PutsStorn:
         self.judge_change_storn = []
     
     def reverse_storn(self, x, y):
+        self.judge_change_storn = []
         #石を置いた位置を保存
         start_x = x
         start_y = y
@@ -31,10 +32,9 @@ class PutsStorn:
         for i in range(8):
             count = judge_reversible_storn(i, x, y, start_x, start_y, self.board, self.turn, self.count)
             self.judge_change_storn.append(reverse_storn_func(i, start_x, start_y, self.board, self.turn, count))
-            print(reverse_storn_func(i, start_x, start_y, self.board, self.turn, count))
             self.count = 0
+
         if any(self.judge_change_storn):
-            print('OK')
             self.board[start_y][start_x] = self.turn
             self.turn *= -1
         else:
